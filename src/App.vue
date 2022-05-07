0c422bcbc2f16359236421c8ed199a1098be52a7
Mahfod <mahfod.dev@gmail.com>
2022-05-07 18:00:12 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+/* eslint-disable vue/html-indent */
+<template>
+	<h1>Ninja reaction timer</h1>
+	<button @click="start" :disabled="isStart">play</button>
+	<Block v-if="isStart" :delay="delay" @end="endGame" />
+	<Results v-if="showScore" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+export default {
+	components: { Block, Results },
+	name: 'App',
+	data() {
+		return {
+			isStart: false,
+			delay: null,
+			score: null,
+			showScore: false,
+		};
+	},
+	methods: {
+		start() {
+			this.isStart = true;
+			this.delay = 2000 + Math.random() * 5000;
+			this.showScore = false;
+			this.score = null;
+		},
+		endGame(reactionTime) {
+			this.isStart = false;
+			this.score = reactionTime;
+			this.showScore = true;
+		},
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #444;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1>Result: {{ score }}</h1>
+	<p>...{{ rank }}...</p>
+</template>
+<script>
+export default {
+	props: ['score'],
+	data() {
+		return {
+			rank: null,
+		};
+	},
+	mounted() {
+		if (this.score < 250) {
+			this.rank = 'Ninja fingers';
+		} else if (this.score < 400) {
+			this.rank = 'Rapid reflex';
+		} else {
+			this.rank = 'Snail pace...';
+		}
+	},
+};
+</script>
